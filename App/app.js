d1c6b4e8851d3b90faf3957acccd645ec7b67675
Brad Goodlett <brgoodlett@gmail.com>
2021-02-10 02:52:51 -0800

    added comments to explain the javascript code

--- App/app.js
+++ App/app.js
@@ -4,12 +4,15 @@ window.onload = () => {
     
     //get processes and select the amazon scraper
     UiPathRobot.getProcesses().then(results => {
+        //throw an error if no processes were found
         if(results.length === 0) {
             throw new Error("No Processes found")
         }
 
+        //select the amazon scraper from the list of processes
         amazonScraper = results.find(res => res.name.includes("Scrape Amazon"))
 
+        //throw an error if the amazon scraper was not found.
         if(!amazonScraper) {
             throw new Error("Amazon Scraper not found")
         }
@@ -17,7 +20,9 @@ window.onload = () => {
         console.err(err.what())
     })
 
+    //add event listener to the form
     document.querySelector("#robot-form").addEventListener("submit", function (event) {
+        //prevent default behavior of form submission to stop a page reload
         event.preventDefault()
         runRobot(amazonScraper)
     }, false)
@@ -25,15 +30,18 @@ window.onload = () => {
 
 const runRobot = (robot) => {
     let args = {
+        //the number of items to be scraped from Amazon
         itemCount: parseInt(document.querySelector('#item-count').value)
     }
 
+    //check if an argument was given and replace with null if none were
     if(!args.itemCount) {
         args = null
     }
 
     console.log(args)
 
+    //start the robot and log the status / results / errors
     robot.start(args)
     .onStatus(status => {
         console.log(status)
